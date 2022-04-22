913d6df4d4d528f8021181d141ad0dac6c840b06
Andy Chan <Chanandy727@gmail.com>
2022-04-22 14:00:50 -0700

    fixed path

--- database/seed.sql
+++ database/seed.sql
@@ -9,21 +9,21 @@ DELIMITER ','
 CSV HEADER;
 
 COPY Styles(id, product_id, name, sale_price, original_price, default_style)
-FROM '/Users/andychan/Downloads/SDC_CSV/features.csv/styles.csv'
+FROM '/Users/andychan/Downloads/SDC_CSV/styles.csv'
 DELIMITER ','
 CSV HEADER;
 
 COPY Photos(id, style_id, url, thumbnail_url)
-FROM '/Users/andychan/Downloads/SDC_CSV/features.csv/photos.csv'
+FROM '/Users/andychan/Downloads/SDC_CSV/photos.csv'
 DELIMITER ','
 CSV HEADER;
 
 COPY Skus(id, style_id, size, quantity)
-FROM '/Users/andychan/Downloads/SDC_CSV/features.csv/skus.csv'
+FROM '/Users/andychan/Downloads/SDC_CSV/skus.csv'
 DELIMITER ','
 CSV HEADER;
 
 COPY Related_Products(id, curr_prod_id, related_prod_id)
-FROM '/Users/andychan/Downloads/SDC_CSV/features.csv/related.csv'
+FROM '/Users/andychan/Downloads/SDC_CSV/related.csv'
 DELIMITER ','
 CSV HEADER;
