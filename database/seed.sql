f88755fb8bdef85e4cfa163c0e3c4a2276f504bb
Andy Chan <Chanandy727@gmail.com>
2022-04-22 13:07:24 -0700

    Added ETL

---
+++ database/seed.sql
@@ -0,0 +1,29 @@
+COPY Products(id, name, slogan, description, category, default_price)
+FROM '/Users/andychan/Downloads/SDC_CSV/product.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY Features(id, product_id, feature, value)
+FROM '/Users/andychan/Downloads/SDC_CSV/features.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY Styles(id, product_id, name, sale_price, original_price, default_style)
+FROM '/Users/andychan/Downloads/SDC_CSV/features.csv/styles.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY Photos(id, style_id, url, thumbnail_url)
+FROM '/Users/andychan/Downloads/SDC_CSV/features.csv/photos.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY Skus(id, style_id, size, quantity)
+FROM '/Users/andychan/Downloads/SDC_CSV/features.csv/skus.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY Related_Products(id, curr_prod_id, related_prod_id)
+FROM '/Users/andychan/Downloads/SDC_CSV/features.csv/related.csv'
+DELIMITER ','
+CSV HEADER;
